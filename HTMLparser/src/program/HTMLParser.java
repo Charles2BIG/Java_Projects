181379daf13aaf08cc2d1dc9188044ee76b64334
Charles2BIG <63507769+Charles2BIG@users.noreply.github.com>
2020-04-30 23:30:42 +0300

    Final working version
    Just a little code refactoring

--- HTMLparser/src/program/HTMLParser.java
+++ HTMLparser/src/program/HTMLParser.java
@@ -6,16 +6,13 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
 
 public class HTMLParser {
-
+    //Метод загружает HTMl страницы, адрес которой передается в url и помещает его в файл
+    //в папку с проектом
     public static void DownloadHtmlFrom(String url) throws java.io.IOException {
         Document html = Jsoup.connect(url)
                 .userAgent("Chrome/4.0.249.0 Safari/532.5")
@@ -26,20 +23,20 @@ public class HTMLParser {
         writer.write(strHTML);
     }
 
+    //Метод принимает путь к файлу с HTML страницы и лист в который необходимо вычленить элементы
+    //HTML кода: текст не относящийся к коду разметки, находящийся в классе по которому происходит сортировка
     public static void ThrowList(String path, ArrayList<String> throwableList) throws java.io.IOException {
         BufferedReader br = new BufferedReader(new FileReader(path));
         String strHTML = null;
         while (br.read() != -1) {
             strHTML += br.readLine();
-            //System.out.println(br.readLine());
         }
         Document doc = Jsoup.parse(strHTML);
-        //System.out.println(doc);
+
         Elements listOfItems = doc.getElementsByClass("selection-film-item-meta__name");
-        Pattern pattern = Pattern.compile("[А-Я[а-я]]+");
         for (Element element : listOfItems) {
-            System.out.println(element);
-            throwableList.add(element.toString());
+            System.out.println(element.text()); // проверка правлиьности сортировки
+            throwableList.add(element.text());
         }
     }
 
